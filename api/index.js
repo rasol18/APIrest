6851f68c3ce50466a2b3a625a341aab51a00ce6b
rasol18 <leonardogarrido1994@gmail.com>
2023-09-27 17:21:50 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 //crea la app//
 const app = express();
 //indica el puerto del servidor//
-const port = 3000;
+const port = process.env.PORT || 3000;
 //crea un middleware
 app.use(express.json());
 //cors funciona como un middleware y asi se llama//
@@ -24,11 +24,11 @@ const options = {
 app.use(cors());
 
 //cuando se entre al servidor, este respondera con un hola mundo//
-app.get('/', (request, response) => {
+app.get('/api', (request, response) => {
    response.send('hello world')
 });
 
-app.get('/nueva-ruta', (request, response) => {
+app.get('/api/nueva-ruta', (request, response) => {
   response.send('hhola este es un nuevo endpoint')
 });
 
@@ -42,7 +42,7 @@ app.use(boombErrorHandler);
 app.use(errorHandler);
 
 //devuelve un producto especifico que esta dentro de una categoria especifica//
-app.get('/categorias/:categoriaId/productos/:productoId',(request, response) => {
+app.get('/api/categorias/:categoriaId/productos/:productoId',(request, response) => {
   const {categoriaId, productoId} = request.params;
 
   response.json({
